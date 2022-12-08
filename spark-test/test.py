99dc5d601fa5de454fcde43fcee213f40187aa53
mahme19 <mahme19@student.sdu.dk>
2022-12-08 19:33:19 +0100

    Added endpoints , need to fix naming in query

--- spark-test/test.py
+++ spark-test/test.py
@@ -21,16 +21,238 @@ servers = "hdfs://namenode:9000/userTest2.json"
 spark = SparkSession.builder.appName('pyspark').getOrCreate()
 
 @app.route('/')
-def root(): 
+def index():
+    return 'Hello World'
+
+@app.route('/mostrepos')
+def most_repos(): 
    
     df = spark.read.json(servers)
     df.createOrReplaceTempView("users")
 
-    most_repos = spark.sql("SELECT username, repos FROM users WHERE repos in (select max(repos) FROM users)")
+    query = spark.sql("SELECT username, repos FROM users WHERE repos in (select max(repos) FROM users)")
 
-    print(most_repos.toJSON())
+    row = query.collect()
+    data = row[0].asDict()
+
+    print(data)
     
-    return json.load(most_repos.toJSON())
+    return json.dumps(data)
+
+
+@app.route('/leastrepos')
+def least_repos():
+    df = spark.read.json(servers)
+    df.createOrReplaceTempView("users")
+
+    query = spark.sql("SELECT username, repos FROM users WHERE repos in (select min(repos) FROM users)")
+
+    row = query.collect()
+    data = row[0].asDict()
+
+    print(data)
+    
+    return json.dumps(data)
+
+@app.route('/mostfollowers')
+def most_followers():
+    df = spark.read.json(servers)
+    df.createOrReplaceTempView("users")
+
+    query = spark.sql("SELECT username, followers FROM users WHERE followers in (select max(followers) FROM users)")
+
+    row = query.collect()
+    data = row[0].asDict()
+
+    print(data)
+    
+    return json.dumps(data)
+
+
+@app.route('/leastfollowers')
+def least_followers():
+    df = spark.read.json(servers)
+    df.createOrReplaceTempView("users")
+
+    query = spark.sql("SELECT username, followers FROM users WHERE followers in (select min(followers) FROM users)")
+
+    row = query.collect()
+    data = row[0].asDict()
+
+    print(data)
+    
+    return json.dumps(data)
+
+
+@app.route('/mostfollowings')
+def most_followings():
+    df = spark.read.json(servers)
+    df.createOrReplaceTempView("users")
+
+    query = spark.sql("SELECT username, following FROM users WHERE following in (select max(following) FROM users)")
+
+    row = query.collect()
+    data = row[0].asDict()
+
+    print(data)
+    
+    return json.dumps(data)
+
+
+@app.route('/leastfollowings')
+def least_followings():
+    df = spark.read.json(servers)
+    df.createOrReplaceTempView("users")
+
+    query = spark.sql("SELECT username, following FROM users WHERE following in (select min(following) FROM users)")
+
+    row = query.collect()
+    data = row[0].asDict()
+
+    print(data)
+    
+    return json.dumps(data)
+
+
+app.route('/moststars')
+def most_stars():
+    
+    df = spark.read.json(servers)
+    df.createOrReplaceTempView("repos")
+
+    query = spark.sql("SELECT Name, Stars FROM repos WHERE Stars in (select max(Stars) FROM repos)")
+
+    row = query.collect()
+    data = row[0].asDict()
+
+    print(data)
+    
+    return json.dumps(data)
+
+
+app.route('/leaststars')
+def least_stars():
+    
+    df = spark.read.json(servers)
+    df.createOrReplaceTempView("repos")
+
+    query = spark.sql("SELECT Name, Stars FROM repos WHERE Stars in (select min(Stars) FROM repos)")
+
+    row = query.collect()
+    data = row[0].asDict()
+
+    print(data)
+    
+    return json.dumps(data)
+
+app.route('/mostwatchers')
+def most_watchers():
+    
+    df = spark.read.json(servers)
+    df.createOrReplaceTempView("repos")
+
+    query = spark.sql("SELECT Name, Watchers FROM repos WHERE Watchers in (select max(Watchers) FROM repos)")
+
+    row = query.collect()
+    data = row[0].asDict()
+
+    print(data)
+    
+    return json.dumps(data)
+
+app.route('/leastwatchers')
+def least_watchers():
+    
+    df = spark.read.json(servers)
+    df.createOrReplaceTempView("repos")
+
+    query = spark.sql("SELECT Name, Watchers FROM repos WHERE Watchers in (select min(Watchers) FROM repos)")
+
+    row = query.collect()
+    data = row[0].asDict()
+
+    print(data)
+    
+    return json.dumps(data)
+
+app.route('/mostforks')
+def most_forks():
+    
+    df = spark.read.json(servers)
+    df.createOrReplaceTempView("repos")
+
+    query = spark.sql("SELECT Name, Forks FROM repos WHERE Forks in (select max(Forks) FROM repos)")
+
+    row = query.collect()
+    data = row[0].asDict()
+
+    print(data)
+    
+    return json.dumps(data)
+
+app.route('/leastforks')
+def least_forks():
+    
+    df = spark.read.json(servers)
+    df.createOrReplaceTempView("repos")
+
+    query = spark.sql("SELECT Name, Forks FROM repos WHERE Forks in (select min(Forks) FROM repos)")
+
+    row = query.collect()
+    data = row[0].asDict()
+
+    print(data)
+    
+    return json.dumps(data)
+
+app.route('/hasnowiki')
+def has_no_wiki():
+    
+    df = spark.read.json(servers)
+    # df.createOrReplaceTempView("repos")
+
+      
+    #  query = spark.sql("SELECT Name, Forks FROM repos WHERE has_wiki = false")
+
+    row = df.filter(df["has_wiki"] == False).collect()
+    
+    
+
+    data = {
+        'amount': row.length
+    }
+
+
+    print(data)
+    
+    return json.dumps(data)
+
+
+@app.route('/haswiki')
+def has_no_wiki():
+    
+    df = spark.read.json(servers)
+    # df.createOrReplaceTempView("repos")
+
+      
+    #  query = spark.sql("SELECT Name, Forks FROM repos WHERE has_wiki = false")
+
+    row = df.filter(df["has_wiki"] == True).collect()
+    
+    
+
+    data = {
+        'amount': row.length
+    }
+
+
+    print(data)
+    
+    return json.dumps(data)
+
+
+
+
 
 
 if __name__ == '__main__':
